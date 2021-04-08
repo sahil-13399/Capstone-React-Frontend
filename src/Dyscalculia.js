2d05bab40e2623f4dadfd2d9ccdc2dcead922409
Sahil Samantaray <sahil13399@gmail.com>
2021-04-08 16:43:58 +0530

    Dyslexia Partial Complete

--- src/Dyscalculia.js
+++ src/Dyscalculia.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import FileUploader from './FileUploader';
 function Dyscalculia() {
+    const [name, setName] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
     return (
-        <div>
-            
-        </div>
-    )
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+  
+         <FileUploader
+          onFileSelectSuccess={(file) => setSelectedFile(file)}
+          onFileSelectError={({ error }) => alert(error)}
+        />
+        {console.log(selectedFile)}
+        </form>
+      </div>
+    );
 }
 
 export default Dyscalculia
--- src/Dyslexia.js
+++ src/Dyslexia.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React,{useState} from 'react'
+import TextField from '@material-ui/core/TextField';
+import './Dyslexia.css'
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 function Dyslexia() {
+    const [fname, setFName] = useState("");
+    const [lname, setLName] = useState("");
+    const [gender, setGender] = useState("");
+    const [nlanguage, setNLanguage] = useState("");
+    const [olanguage, setOLanguage] = useState("");
+    const [age, setAge] = useState("");
+    const UPLOAD_URL = "https://httpbin.org/post"
+    const [selectedFile, setSelectedFile] = useState(null);
+    const handleFileInput = (e) => {
+        // handle validations
+        const file = e.target.files[0];
+        //console.log(file)
+       setSelectedFile(file)
+      };
+    const submitForm = (e) => {
+        e.preventDefault() 
+     
+        //setSelectedFile(e.target.files[0])
+        const formData = new FormData();
+        formData.append("gender", gender);
+        formData.append("nlanguage", nlanguage);
+        formData.append("olanguage", olanguage);
+        formData.append("age", age);
+        formData.append("file", selectedFile);
+        console.log(formData.getAll)
+        console.log(gender);
+        axios
+          .post(UPLOAD_URL, formData)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => alert("Post request error"));
+      };
+  
     return (
-        <div>
+      <div className="Dyslexia" >
+        <form>
+            <TextField id="standard-basic" label="First name" value={fname} onChange={(e) => setFName(e.target.value)}/>
+            <TextField id="standard-basic" label="Last name" value={lname} onChange={(e) => setLName(e.target.value)}/>
+            <TextField id="standard-basic" label="Gender" value={gender} onChange={(e) => setGender(e.target.value)}/>
+            <TextField id="standard-basic" label="Native Language" value={nlanguage} onChange={(e) => setNLanguage(e.target.value)}/>
+            <TextField id="standard-basic" label="Other Language" value={olanguage} onChange={(e) => setOLanguage(e.target.value)}/>
+            <TextField id="standard-basic" label="Age" value={age} onChange={(e) => setAge(e.target.value)}/>
             
-        </div>
-    )
+            
+        <input style={{padding:10}} type="file" onChange={handleFileInput} />
+        <Button type="submit" onClick={submitForm} variant="outlined" color="primary">Submit</Button>
+        
+        </form>
+      </div>
+    );
 }
 
 export default Dyslexia

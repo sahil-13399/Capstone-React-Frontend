a722585bae2efd54b306236a20e2a669fb17ba52
sahil <sahil13399@gmail.com>
2021-04-12 16:19:13 +0530

    Form implementation of Dyscalculia

--- src/Dyscalculia.js
+++ src/Dyscalculia.js
@@ -2,41 +2,101 @@ import React,{useState} from 'react'
 import TextField from '@material-ui/core/TextField';
 import image_compare from './static/Image_compare.png'
 import image_count from './static/image_count.png'
-import image_time1 from './static/image_time1.png'
+import image_compute from './static/image_compute.png'
+import image_time from './static/image_time.png'
+import image_geo from './static/image_geometry.png'
 import { Button } from '@material-ui/core';
+import axios from 'axios';
+
 
 function Dyscalculia() {
     const [comp1, setComp1] = useState("");  
     const [comp2, setComp2] = useState("");
     const [comp3, setComp3] = useState("");
     const [comp4, setComp4] = useState("");
+    const [count1,setCount1] = useState("");
+    const [count2,setCount2] = useState("");
+    const [count3,setCount3] = useState("");
+    const [count4,setCount4] = useState("");
+    const [compute1,setCompute1] = useState("");
+    const [compute2,setCompute2] = useState("");
+    const [compute3,setCompute3] = useState("");
+    const [compute4,setCompute4] = useState("");
+    const [time1,setTime1] = useState("");
+    const [time2,setTime2] = useState("");
+    const [time3,setTime3] = useState("");
+    const [time4,setTime4] = useState("");
+    const [geo1,setGeo1] = useState("");
+    const [geo2,setGeo2] = useState("");
+    const [geo3,setGeo3] = useState("");
+    const [geo4,setGeo4] = useState("");
     const [counter,setCounter] = useState(0);
+    const submitForm = (e) => {
+      e.preventDefault() 
+   
+      //setSelectedFile(e.target.files[0])
+      const formData = new FormData();
+      formData.append('comp1',comp1);
+      formData.append('comp2',comp2);
+      formData.append('comp3',comp3);
+      formData.append('comp4',comp4);
+    };
     return (
       <div className="Dyscalculia">
         <form>
           {counter === 0 && <div className="compare" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
-            <img src={image_compare} alt="Image location invalid"/>
-            <TextField style={{width:200}} id="standard-basic" label="1. " value={comp1} onChange={(e) => setComp1(e.target.value)}/>
+            <img src={image_compare} alt="location invalid"/>
+            <TextField style={{width:200,marginTop:30}} id="standard-basic" label="1. " value={comp1} onChange={(e) => setComp1(e.target.value)}/>
             <TextField style={{width:200}} id="standard-basic" label="2. " value={comp2} onChange={(e) => setComp2(e.target.value)}/>
             <TextField style={{width:200}} id="standard-basic" label="3. " value={comp3} onChange={(e) => setComp3(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="4. " value={comp4} onChange={(e) => setComp4(e.target.value)}/>
-            <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            <TextField style={{width:200,marginBottom:30}} id="standard-basic" label="4. " value={comp4} onChange={(e) => setComp4(e.target.value)}/>
+            <div className="Button" style={{display:'flex'}}>
+              <Button variant="contained" color="primary" style={{width:100,right:-600}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            </div>
           </div>}
           {counter === 1 && <div className="counting" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
-            <img src={image_count} alt="Image location invalid"/>
-            <TextField style={{width:200}} id="standard-basic" label="1. " value={comp1} onChange={(e) => setComp1(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="2. " value={comp2} onChange={(e) => setComp2(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="3. " value={comp3} onChange={(e) => setComp3(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="4. " value={comp4} onChange={(e) => setComp4(e.target.value)}/>
-            <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            <img src={image_count} alt="location invalid"/>
+            <TextField style={{width:200,marginTop:30}} id="standard-basic" label="1. " value={count1} onChange={(e) => setCount1(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="2. " value={count2} onChange={(e) => setCount2(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="3. " value={count3} onChange={(e) => setCount3(e.target.value)}/>
+            <TextField style={{width:200,marginBottom:30}} id="standard-basic" label="4. " value={count4} onChange={(e) => setCount4(e.target.value)}/>
+            <div className="Button" style={{display:'flex'}}>
+              <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter-1)}>Prev</Button>
+              <Button variant="contained" color="primary" style={{width:100,right:-600}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            </div>
           </div>}
-          {counter === 2 && <div className="counting" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
-            <img src={image_count} alt="Image location invalid"/>
-            <TextField style={{width:200}} id="standard-basic" label="1. " value={comp1} onChange={(e) => setComp1(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="2. " value={comp2} onChange={(e) => setComp2(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="3. " value={comp3} onChange={(e) => setComp3(e.target.value)}/>
-            <TextField style={{width:200}} id="standard-basic" label="4. " value={comp4} onChange={(e) => setComp4(e.target.value)}/>
-            <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter+1)}>Next</Button>
+          {counter === 2 && <div className="compute" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
+            <img src={image_compute} alt="location invalid"/>
+            <TextField style={{width:200,marginTop:30}} id="standard-basic" label="1. " value={compute1} onChange={(e) => setCompute1(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="2. " value={compute2} onChange={(e) => setCompute2(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="3. " value={compute3} onChange={(e) => setCompute3(e.target.value)}/>
+            <TextField style={{width:200,marginBottom:30}} id="standard-basic" label="4. " value={compute4} onChange={(e) => setCompute4(e.target.value)}/>
+            <div className="Button" style={{display:'flex'}}>
+              <Button variant="contained" color="primary" style={{width:100,right:-490}} onClick={()=>setCounter(counter-1)}>Prev</Button>
+              <Button variant="contained" color="primary" style={{width:100,right:-520}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            </div>
+          </div>}
+          {counter === 3 && <div className="time" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
+            <img src={image_time} style={{height:500}} alt="location invalid"/>
+            <TextField style={{width:200,marginTop:30}} id="standard-basic" label="1. " value={time1} onChange={(e) => setTime1(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="2. " value={time2} onChange={(e) => setTime2(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="3. " value={time3} onChange={(e) => setTime3(e.target.value)}/>
+            <TextField style={{width:200,marginBottom:30}} id="standard-basic" label="4. " value={time4} onChange={(e) => setTime4(e.target.value)}/>
+            <div className="Button" style={{display:'flex'}}>
+              <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter-1)}>Prev</Button>
+              <Button variant="contained" color="primary" style={{width:100,right:-600}} onClick={()=>setCounter(counter+1)}>Next</Button>
+            </div>
+          </div>}
+          {counter === 4 && <div className="geometry" style={{display:'flex',flexDirection:'column',justifyContent:'space-between'}}>
+            <img src={image_geo} style={{height:500}} alt="location invalid"/>
+            <TextField style={{width:200,marginTop:30}} id="standard-basic" label="1. " value={geo1} onChange={(e) => setGeo1(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="2. " value={geo2} onChange={(e) => setGeo2(e.target.value)}/>
+            <TextField style={{width:200}} id="standard-basic" label="3. " value={geo3} onChange={(e) => setGeo3(e.target.value)}/>
+            <TextField style={{width:200,marginBottom:30}} id="standard-basic" label="4. " value={geo4} onChange={(e) => setGeo4(e.target.value)}/>
+            <div className="Button" style={{display:'flex'}}>
+              <Button variant="contained" color="primary" style={{width:100,right:-570}} onClick={()=>setCounter(counter-1)}>Prev</Button>
+              <Button variant="contained" color="primary" style={{width:100,right:-600}} onClick={()=>setCounter(counter+1)}>Submit</Button>
+            </div>
           </div>}
         </form>
       </div>
